ec1ce4c9e1bd7c784944ad9b7d01c0caf0640af0
Rogers <r.teles@live.com>
2025-09-20 17:04:47 -0300

    Update app.module.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,17 +1,14 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { ControllersModule } from './gateways/controllers/controllers.module';
 import { DomainModule } from './domain/domain.module';
 import { InfrastructureModule } from './infrastructure/infrastructure.module';
 import { GatewaysModule } from './gateways/gateways.module';
-import { GetAllProjectsService } from './–flat/domain/use-cases/projects/get-all-projects/get-all-projects.service';
-import { GetAllProjectsService } from './–flat/domain/use-cases/projects/get-all-projects/get-all-projects.service';
 
 @Module({
-  imports: [ControllersModule, DomainModule, InfrastructureModule, GatewaysModule],
+  imports: [DomainModule, InfrastructureModule, GatewaysModule],
   controllers: [AppController],
-  providers: [AppService, GetAllProjectsService],
+  providers: [AppService],
 })
 export class AppModule {}
 
